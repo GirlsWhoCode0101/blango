eb8bb0e295ac5f20a9ca4ce0a1c1fdf985a70aab
Janine Sukowski <janine_byapple@me.com>
2022-07-31 09:50:09 +0000

    Django admin

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.value
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True,  db_index=True)
+    modified_at = models.DateTimeField(auto_now=True,  db_index=True)
+    published_at = models.DateTimeField(blank=True, null=True, db_index=True)
+
+    title = models.TextField(max_length=100)
+    slug = models.SlugField(unique=True)
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+
+    tags = models.ManyToManyField(Tag, related_name="posts")
